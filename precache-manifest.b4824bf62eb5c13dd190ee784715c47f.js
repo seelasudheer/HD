6bbdbcd568f2622731022e714939ce3d48e6adfb
Sudheer Seela <ssudheer@corp.ojas-it.com>
2019-12-09 18:49:06 +0530

    Updates

---
+++ precache-manifest.b4824bf62eb5c13dd190ee784715c47f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef1ea41e3d4e68be1ac1864f466028dd",
+    "url": "/seelasudheer/remo.git/index.html"
+  },
+  {
+    "revision": "1f4de4a6773fa3613dd2",
+    "url": "/seelasudheer/remo.git/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "604a1e749ec93c4675dc",
+    "url": "/seelasudheer/remo.git/static/js/2.bb663b7f.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/seelasudheer/remo.git/static/js/2.bb663b7f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1f4de4a6773fa3613dd2",
+    "url": "/seelasudheer/remo.git/static/js/main.9db0ef30.chunk.js"
+  },
+  {
+    "revision": "77ca91895dcc77740ff6",
+    "url": "/seelasudheer/remo.git/static/js/runtime-main.c2605386.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/seelasudheer/remo.git/static/media/logo.5d5d9eef.svg"
+  }
+]);
